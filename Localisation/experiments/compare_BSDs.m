47c2895c4c92d588ef8009c6156942efa212fa61
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-09-16 15:10:56 +0100

    rearrangement

--- Localisation/experiments/compare_BSDs.m
+++ Localisation/experiments/compare_BSDs.m
@@ -1,4 +1,12 @@
 % compare BSDs
-load('london_BSD_2degree.mat');
+clear all
+close all
+load('london_BSD_new75_small.mat');
 routes2 = routes;
-load('london_BSD.mat');
+load('routes_small_withBSD_75.mat');
+diff_count = 0;
+for i=1:length(routes)
+    if routes(i).BSDs ~= routes2(i).BSDs
+        diff_count = diff_count+1;
+    end    
+end
