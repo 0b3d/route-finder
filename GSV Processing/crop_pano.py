3ead79db319c8bf75e376a621e0d8d97cf6ed4e0
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-09-18 21:55:54 +0100

    download gsv images

--- GSV Processing/crop_pano.py
+++ GSV Processing/crop_pano.py
@@ -2,6 +2,7 @@ import numpy as np
 import os, sys
 import cv2
 import Equirec2Perspec as E2P
+from scipy.io import loadmat
 
 def crop_pano(pano):
     # Crop gsv 
@@ -14,13 +15,56 @@ def crop_pano(pano):
     equ = E2P.Equirectangular(pano)
 
     for i,tetha in enumerate([0,-90,90,180]):
-        snap = equ.GetPerspective(100, tetha, 0, 256, 256)  
+        snap = equ.GetPerspective(100, tetha, 0, 227, 227)  
         snaps.append(snap) # Must have shape [1, 224,224,3]
     
     return snaps 
 
 
 if __name__ == '__main__':
-    path = os.path.join(os.getcwd(), 'Data',sys.argv[1], 'panos', sys.argv[2])
-    pano = cv2.imread(path)
-    snaps = crop_pano(pano)
+    # path = os.path.join(os.getcwd(), 'Data',sys.argv[1], 'panos', sys.argv[2])
+    directory = os.getcwd()
+    snaps_directory = os.path.join(directory, 'Data', 'snaps') 
+    if not os.path.isdir(snaps_directory):
+         os.mkdir(snaps_directory)
+
+    # Open matlab file
+    # routes_file = os.path.join(os.getcwd(), 'Data', sys.argv[1], 'routes_small.mat')
+    routes_file = os.path.join(os.getcwd(), 'Data', 'routes_small.mat')
+    test = loadmat(routes_file)
+    routes = test['routes'].squeeze()
+    # 0 -> coords
+    # 1 yaw
+    # 2 wayidx
+    # 3 neighbor 
+    # 4 pano_id 
+    # 5 gsv_coords 
+    # 6 gsv_yaw
+
+    # Read all the pano_ids to process
+    pano_ids = []
+    for i in range(routes.shape[0]):
+        pano_id = routes[i][4].squeeze()
+        pano_ids.append(str(pano_id))
+
+    for k, pano_id in enumerate(pano_ids):
+        path = os.path.join(os.getcwd(), 'Data', 'panos', pano_id + '.jpg')
+        pano = cv2.imread(path)
+        snaps = crop_pano(pano)
+        filename = os.path.join(snaps_directory,  pano_id + '.jpg') 
+        filename_f = os.path.join(snaps_directory, pano_id + '_front.jpg')
+        cv2.imwrite(filename_f, snaps[0]) 
+        filename_l = os.path.join(snaps_directory, pano_id + '_left.jpg')
+        cv2.imwrite(filename_l, snaps[1]) 
+        filename_r = os.path.join(snaps_directory, pano_id + '_right.jpg')
+        cv2.imwrite(filename_r, snaps[2]) 
+        filename_b = os.path.join(snaps_directory, pano_id + '_back.jpg')
+        cv2.imwrite(filename_b, snaps[3])    
+        msg = filename + " saved"
+        print(pano_id, msg)
+        # for visualization
+        # snaps = np.concatenate(snaps, axis=1)
+        # cv2.imshow('snaps', snaps)
+        # cv2.waitKey(0)
+        # if k == 10:
+        #    break
--- GSV Processing/download_panos.py
+++ GSV Processing/download_panos.py
@@ -7,7 +7,7 @@ import multiprocessing
 from subprocess import call
 import numpy as np
 from scipy.io import loadmat
-import tables
+#import tables
 import cv2
 import os, sys
 import h5py
@@ -119,7 +119,6 @@ if not os.path.isdir(panos_directory):
 routes_file = os.path.join(os.getcwd(), 'Data', 'routes_small.mat')
 test = loadmat(routes_file)
 routes = test['routes'].squeeze()
-
 # 0 -> coords
 # 1 yaw
 # 2 wayidx
