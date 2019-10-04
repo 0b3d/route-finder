9380898a301f6cd1b2ae6c15e13e9874301070b8
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-10-04 17:01:54 +0100

    add test

--- parameters.m
+++ parameters.m
@@ -1,6 +1,6 @@
-mapfile = 'oxford.osm';
-dataset = 'oxford_09_19'; %The name of the dataset, creates a folder in /Data
-test_num = 300; % The number of test routes
+mapfile = 'test.osm';
+dataset = 'test'; %The name of the dataset, creates a folder in /Data
+test_num = 10; % The number of test routes
 max_route_length_init = 20; % the lenght of the routes
 threshold = 60; % turn threshold
 road_dense_distance = 10;
