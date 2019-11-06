cda52fa2b1bae654f49e769d5a3a51fa59125c16
Mengjie Zhou <mengjiezhou2018@outlook.com>
2019-11-06 20:14:45 +0000

    update results

--- parameters.m
+++ parameters.m
@@ -1,5 +1,5 @@
-mapfile = 'luton_v4.osm';
-dataset = 'luton_v4'; %The name of the dataset, creates a folder in /Data
+mapfile = 'london.osm';
+dataset = 'london_10_19'; %The name of the dataset, creates a folder in /Data
 test_num = 500; % The number of test routes
 max_route_length_init = 40; % the lenght of the routes
 threshold = 60; % turn threshold
@@ -14,7 +14,7 @@ s_number = 5; % 5 successive locations
 % parameters for BSD freatures
 radius = 35; % search radius is 35m
 thresh = 10; % filter inters if their angles is below 10 degree
-accuracy = 0.8;
+accuracy = 1;
 range = 2; % generate rays every _degree
 thresh_jc = 30; % 30m
 thresh_bd = 3;  % 4 degree
@@ -22,5 +22,5 @@ thresh_bd = 3;  % 4 degree
 
 % choose features type
 features_type = 'BSD'; % 'BSD' 'ES' or 'none'
-turns = 'true'; % 'true', 'false', 'only'
+turns = 'false'; % 'true', 'false', 'only'
 probs = 'false'; % for 'BSD', set this to 'false'
