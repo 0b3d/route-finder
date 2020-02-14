780d7769db0d7200460f7753b1bd25c0c1945df8
0b3d <samanoabonce@gmail.com>
2020-02-14 12:53:24 +0000

    Added turns vs accuracy

--- ESparams.m
+++ ESparams.m
@@ -1,31 +1,3 @@
-mapfile = 'pittsburgh.osm';
-dataset = 'unionsquare5k'; %The name of the dataset, creates a folder in /Data
-test_num = 500; % The number of test routes
-max_route_length_init = 40; % the lenght of the routes
-threshold = 60; % turn threshold
-road_dense_distance = 10;
-% drop threshold for routes
-N = [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,...
-    100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100];
-% consistency metric
-overlap = 0.8; % 80%
-s_number = 5; % 5 successive locations
-
-% parameters for BSD freatures
-radius = 35; % search radius is 35m
-thresh = 10; % filter inters if their angles is below 10 degree
-accuracy = 0.75;
-accuracy_jcf = 1;
-accuracy_bdr = 0.8;
-accuracy_jcb = 1;
-accuracy_bdl = 0.8;
-
-range = 2; % generate rays every _degree
-thresh_jc = 50; % 30m
-thresh_bd = 3;  % 4 degree
-thresh_dist = 5; % 5m
-
-
 % choose features type
 params = struct 
 params.features_type = 'ES'
---
+++ utils/turnsvsaccuracy.m
@@ -0,0 +1,29 @@
+ESparams;
+load(params.ESResultsPath, 'ranking', 'test_route')
+
+m = 20;
+k = 1
+acc = zeros(4,1);
+count = zeros(4,2)
+
+for th=0:4
+    routes_subset_indices = find(sum(test_turn(:,1:m),2) >= th);
+    routes_subset_rankings = ranking(routes_subset_indices,m);
+    num_routes_subset = size(routes_subset_rankings, 1);
+    num_routes_top1 = size(find(routes_subset_rankings(:,1) <= k),1);
+    %count(th,:) = [size(routes_subset_rankings, 1), size(find(routes_subset_rankings(:,20) == 1), 1)];
+    count(th+1,:) = [num_routes_top1, num_routes_subset - num_routes_top1];
+    acc(th+1,1) = num_routes_top1 / num_routes_subset;
+end
+    
+b = bar(count, 'stack')
+xtips1 = b(1).XEndPoints;
+ytips1 = b(1).YEndPoints;
+labels1 = string(acc);
+text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
+    'VerticalAlignment','bottom')
+legend({'Localised routes', 'Not localised routes'})
+xlabel('Number of turns in test route')
+ylabel('Number of routes in testing subset')
+xticklabels({'0','1','2','3','4'})
+title('Localisation accuracy (route length=20)')
