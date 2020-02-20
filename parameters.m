fe7ffc93a1b222a4079277ed87f4c39981dd9b20
Mengjie Zhou <mengjiezhou2018@outlook.com>
2020-02-20 17:24:37 +0000

    new results with turns

--- parameters.m
+++ parameters.m
@@ -38,5 +38,5 @@ max_rays = 43; % 90/2 - 2
 features_type = 'BSD'; % 'BSD' 'ES' or 'none'
 model = 'v1';
 tile_test_zoom = 'z19' ;
-turns = 'true'; % 'true', 'false', 'only'
+turns = 'false'; % 'true', 'false', 'only'
 probs = 'false'; % for 'BSD', set this to 'false'
