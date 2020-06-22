c89f7701aebde12900e167145018e0b6c18912d3
0b3d <samanoabonce@gmail.com>
2020-06-22 20:37:24 +0100

    modified data generation and difference over union

--- Localisation/data_generation.m
+++ Localisation/data_generation.m
@@ -7,15 +7,15 @@ parameters;
 path =  fullfile(pwd);
 addpath(genpath(path));
 load(['features/ES/',model,'/',tile_test_zoom, '/',  dataset,'.mat']); % Load features
-load(['Data/streetlearn/',dataset,'.mat']);
+load(['Data/streetlearn/',dataset,'_new.mat']);
 
 %% generate delete set
-for i=1:length(pano_id)
-        if index(1,i) == routes(i).oidx
-            routes(i).x = X(i,:);
-            routes(i).y = Y(i,:);
-        else
-            error("Indices not match, check files ...")
-        end
+for i=1:5000
+        %if index(1,i) == routes(i).oidx
+        routes(i).x = X(i,:);
+        routes(i).y = Y(i,:);
+        %else
+        %    error("Indices not match, check files ...")
+        %end
 end
 save(['features/ES/', model,'/' , tile_test_zoom, '/','ES_',dataset,'.mat'],'routes');
